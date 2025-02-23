94571a00b36b9430572e77b9f8e54fc07245e53e
sarthakpawar0912 <pawarsr06@gmail.com>
2025-02-23 11:02:31 +0530

    Updated feature or bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,18 @@ import { ViewPostComponent } from './pages/view-post/view-post.component';
 import { SearchByNameComponent } from './pages/search-by-name/search-by-name.component';
 
 const routes: Routes = [
+
   { path: 'create-post', component: CreatePostComponent }, 
+ 
   { path: 'view-all', component: ViewAllComponent }, 
+  
   { path: 'search-by-name', component: SearchByNameComponent}, 
+  
   { path: 'view-post/:id', component: ViewPostComponent }, 
 ];
 
 @NgModule({
+  
   imports: [RouterModule.forRoot(routes)], 
   exports: [RouterModule]
 })
